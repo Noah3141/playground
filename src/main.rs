5044e9eb11aca47662db7854c8a82d85890ad408
Noah S <Noah3141@gmail.com>
2023-04-25 17:46:59 -0400

    file moves

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,18 @@
 mod examples;
 use examples::{*};
 
-use lib::*; // lib is imported modular-style in the cargo.toml file, and now we can use any file::function in the lib/src folder
+use lib::*; 
+/* 
+lib is imported modular-style in the cargo.toml file, and now we can use any file::function in the lib/src folder, such as "rocket::launch()" ,
+if that file is adjacent to lib.rs, and imported inside lib with "pub mod rocket"
+In the current structure, rocket is actually a folder-module, containing its hub "mod.rs", and other files. 
+This means we need lib::rocket::task::tester()
+*/ 
 
 
 fn main() {
 
-    rocket::tester();
+    rocket::task::ignite();
 
 }
 
